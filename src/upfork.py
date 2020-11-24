dd0be54678eab8cf2c0d8198ad2481a41c23f803
LambdaXymox <lambda.xymox@gmail.com>
2020-11-23 22:26:46 -0600

    Restructure git repo scanner to be able to scan for remote labels other than 'origin' and 'myorigin'. It can now scan for any remote repo location urls, with origin as a special case treated separately.

--- src/upfork.py
+++ src/upfork.py
@@ -1,11 +1,16 @@
-import collections
 import os
 import os.path
 import subprocess
 import sys
 
 
-Repository  = collections.namedtuple('Repository', ['name', 'origin', 'myorigin'])
+class Repository:
+    def __init__(self, name, origin_url, remote_urls):
+        self.name = name
+        self.origin_url = origin_url
+        self.remote_urls = remote_urls
+
+
 
 def is_git_repo(name):
     owd = os.getcwd()
@@ -29,49 +34,71 @@ def is_git_repo(name):
     return ret
 
 
-def get_git_url_by_name(name):
+def get_url_by_label(label):
     result = subprocess.run(
-        ['git', 'remote', 'get-url', name], 
+        ['git', 'remote', 'get-url', label], 
         capture_output=True, 
         text=True
     )
     if result.returncode == 0:
         return result.stdout.rstrip()
     else:
-        raise ValueError(f'The git repository does not have a URL named {name}')
+        raise ValueError(f'The git repository does not have a URL named {label}')
 
 
-def scan_repo_root(repo_root):
+def remote_url_labels():
+    result = subprocess.run(
+        ['git', 'remote'],
+        capture_output=True,
+        text=True
+    )
+    remote_labels = result.stdout.rstrip().split('\n')
+
+    return remote_labels
+
+
+def remote_urls(labels, exclude=['origin']):
+    urls = {}
+    for label in (label for label in labels if label not in exclude):
+        try:
+            url = get_url_by_label(label)
+        except ValueError:
+            url = ''
+
+        urls[label] = url
+
+    return urls
+ 
+
+def scan_repo_root(repository_root):
     owd = os.getcwd()
-    os.chdir(repo_root)
-    repos = dict(
-        repo_root = repo_root,
-        repos = {}
+    os.chdir(repository_root)
+    repositories = dict(
+        repository_root = repository_root,
+        repositories = {}
     )
     for name in (name for name in os.listdir('.') if is_git_repo(name)):
         os.chdir(name)
         try:
-            origin_url = get_git_url_by_name('origin')
+            origin_url = get_url_by_label('origin')
         except ValueError:
             origin_url = ''
         
-        try:
-            myorigin_url = get_git_url_by_name('myorigin')
-        except ValueError:
-            myorigin_url = ''
+        labels = remote_url_labels()
+        urls = remote_urls(labels)
 
-        repo = Repository(name, origin_url, myorigin_url)
-        repos['repos'][name] = repo
+        repo = Repository(name, origin_url, urls)
+        repositories['repositories'][name] = repo
         
         os.chdir('..')
 
     os.chdir(owd)
 
-    return repos
+    return repositories
 
 
 def usage():
-    return 'USAGE: upfork /path/to/git/repository/forks/'
+    return 'USAGE: upfork [list | update-local | update-remote] /path/to/git/repository/forks/'
 
 
 def main():
@@ -82,15 +109,15 @@ def main():
     
     if not os.path.exists(repo_root):
         sys.exit(f'Path does not exist: {repo_root}')
-    
+                         
     repos = scan_repo_root(repo_root)
-    for repo in repos['repos'].values():
+    for repo in repos['repositories'].values():
         repo_path = os.path.join(repo_root, repo.name)
         print(f'REPO: {repo_path}')
-        print(f'ORIGIN: {repo.origin}')
-        print(f'MYORIGIN: {repo.myorigin}')
+        print(f'ORIGIN: {repo.origin_url}')
+        print(f'MYORIGIN: {repo.remote_urls}')
 
-    print(len(repos['repos']))
+    print(len(repos['repositories']))
 
 
 if __name__ == 'main':
